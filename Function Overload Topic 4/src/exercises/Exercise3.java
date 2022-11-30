559f2485349a45f961b10c3b9177305e65a28031
aleleotta <aleleotta@106-02.info2>
2022-11-30 14:33:56 +0100

    Exercise 3

--- Function Overload Topic 4/src/exercises/Exercise3.java
+++ Function Overload Topic 4/src/exercises/Exercise3.java
@@ -3,8 +3,45 @@ import java.util.*;
 
 public class Exercise3 {
 	public static void main(String[] args) {
+		int a, b, c;
+		int option;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Options:\n1 = Numbers below said number will sum.\n2 = Sets an interval between 2 numbers and sums all numbers inside the interval.");
+		System.out.print("Introduce an option: ");
+		option = sc.nextInt();
+		switch (option) {
+		case 1:
+			System.out.print("Introduce a number: ");
+			a = sc.nextInt();
+			System.out.println("Result = " + sumInt(a));
+			break;
+		case 2:
+			System.out.print("Introduce first number: ");
+			b = sc.nextInt();
+			System.out.print("Introduce second number: ");
+			c = sc.nextInt();
+			System.out.println("Result = " + sumInt(b, c));
+			break;
+		default:
+			System.out.println("ERROR!");
+		}
 		sc.close();
+	}
+	
+	static int sumInt(int a) {
+		int result = 0;
+		for (int i = 0; i <= a; i++) {
+			result += i ;
+		}
+		return result;
+	}
+	
+	static int sumInt(int b, int c) {
+		int result = 0;
+		for (int i = b + 1; i < c; i++) {
+			result += i ;
+		}
+		return result;
 	}
 }
 /*Diseña dos funciones de nombre sumaEnteros que hagan lo siguiente:
