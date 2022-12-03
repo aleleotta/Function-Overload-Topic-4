fd05d31be77d92227ff5ef021ff68d43c642a239
aless <aless@HPaleleotta>
2022-12-03 09:36:34 +0100

    .

--- Function Overload Topic 4/src/exercises/Exercise5.java
+++ Function Overload Topic 4/src/exercises/Exercise5.java
@@ -3,10 +3,31 @@ import java.util.*;
 
 public class Exercise5 {
 	public static void main(String[] args) {
+		int option, a, b, c, d, e, f;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Select an option: ");
+		option = sc.nextInt();
+		switch(option) {
+		case 1:
+			System.out.print("Introduce a number: ");
+			a = sc.nextInt();
+			System.out.println(randomNumbers(a));
+		case 2:
+		case 3:
+		default:
+		}
 		sc.close();
 	}
-	static int randomNumbers(){}
+	static int randomNumbers(int a){
+		int n = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i <= a ; i++) {
+			n = (int)(Math.random() * 1);
+			System.out.print(n);
+		}
+		sc.close();
+		return n;
+	}
 	static int randomNumbers(){}
 	static int randomNumbers(){}
 }
@@ -15,7 +36,7 @@ a) La primera de las funciones recibirá un argumento de tipo entero que indicar
    Esta función generará números aleatorios comprendidos entre 0 y 1 y los mostrará por pantalla.
 b) La segunda de las funciones recibirá dos argumentos de tipo entero: 
    El primero indica la cantidad de números aleatorios a generar
-   El segundo indica el valor máximo que tomará el número aleatorio. 
+   El segundo indica el valor máximo que tomará el número aleatorio.
    Los números aleatorios generados deben estar entre 0 y el valor máximo introducido por parámetro.
    En este caso los números aleatorios deben ser enteros. Los números aleatorios generados deben mostrarse por pantalla.
 c) La tercera de las funciones recibirá tres argumentos de tipo entero: 
